f156c43f07477ab22de67e0c6688baabc1a6fe23
Edgars-Burtnieks <edgars.burtnieks3@gmail.com>
2017-08-01 14:46:38 +0300

    Displaying news bodies in home page.

--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -3,6 +3,8 @@
 <ul>
     {% for new in news %}
     <li><a href="news/{{new.news_id}}">{{ new.title }}</a> | <small>{{ new.author }} | {{ new.created_at }}</small></li>
+
+    <p>{{ new.body|length > 50 ? new.body|slice(0, 200) ~ '...' : new.body  }}</p>
     {% endfor %}
 </ul>
 
