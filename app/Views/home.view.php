cbc6fdac324d4aa61fdc5a45bf6d5ced6bcf5569
Evita Sivakova <evita.sivakova@gmail.com>
2017-08-01 16:52:41 +0300

    visual changes for home page

--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -1,22 +1,30 @@
 {% include 'header.php' %}
 
-<h1>Our Latest News</h1>
+<div class="row">
 
-<ul>
-    {% for new in news %}
-    <li><a href="news/{{new.news_id}}">{{ new.title }}</a> | <small><i>{{ new.author }} | {{ new.created_at }}</i></small></li>
+    <div class="col-md-8">
+        <h1>Our Latest News</h1>
 
-    <p>{{ new.body|length > 200 ? new.body|slice(0, 200) ~ '...' : new.body  }}</p>
-    {% endfor %}
-</ul>
+        <ul>
+            {% for new in news %}
+            <li><a href="news/{{new.news_id}}">{{ new.title }}</a> | <small><i>{{ new.author }} | {{ new.created_at }}</i></small></li>
 
-<h1>Category</h1>
-<ul>
-{% for category in categories %}
-    <li><a href="category/{{category.category_id}}">{{category.title}}</a> </li>
+            <p>{{ new.body|length > 200 ? new.body|slice(0, 200) ~ '...' : new.body  }}</p>
+            {% endfor %}
+        </ul>
+    </div>
 
-{% endfor %}
-</ul>
+    <div class="col-md-4">
+        <h1>Category</h1>
+        <ul>
+            {% for category in categories %}
+            <li><a href="category/{{category.category_id}}">{{category.title}}</a> </li>
+
+            {% endfor %}
+        </ul>
+    </div>
+
+</div>
 
 
 {% include 'footer.php' %}
