876c131584ffefc96d5dbb3b1e950e8741e7c5c8
Edgars-Burtnieks <edgars.burtnieks3@gmail.com>
2017-08-01 15:22:53 +0300

    Added titles for the pages and news body in /views page.

--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -1,10 +1,12 @@
 {% include 'header.php' %}
 
+<h1>Our Latest News</h1>
+
 <ul>
     {% for new in news %}
-    <li><a href="news/{{new.news_id}}">{{ new.title }}</a> | <small>{{ new.author }} | {{ new.created_at }}</small></li>
+    <li><a href="news/{{new.news_id}}">{{ new.title }}</a> | <small><i>{{ new.author }} | {{ new.created_at }}</i></small></li>
 
-    <p>{{ new.body|length > 50 ? new.body|slice(0, 200) ~ '...' : new.body  }}</p>
+    <p>{{ new.body|length > 200 ? new.body|slice(0, 200) ~ '...' : new.body  }}</p>
     {% endfor %}
 </ul>
 
