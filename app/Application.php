d23a7bace822fd2a57260d404b8d1d073813a66e
Edgars-Burtnieks <edgars.burtnieks3@gmail.com>
2017-08-02 17:45:47 +0300

    email fix in application

--- app/Application.php
+++ app/Application.php
@@ -25,7 +25,6 @@ class Application
             $categories = new CategoriesController($this->getContainer());
             $news = new NewsController($this->getContainer());
             $staticPage = new StaticPageController($this->getContainer());
-            $email = new EmailController($this->getContainer());
 
             $r->addRoute('GET', '/', [$home, 'homeAction']);
             $r->addRoute('GET', '/categories', [$categories, 'CategoryAction']);
@@ -39,7 +38,7 @@ class Application
             $r->addRoute('POST', '/news/{id}/new-comment', [$news, 'newCommentAction']);
             $r->addRoute('GET', '/about', [$staticPage, 'aboutAction']);
             $r->addRoute('GET', '/contact', [$staticPage, 'contactAction']);
-            $r->addRoute('POST', '/send-email', [$email, 'emailAction']);
+            $r->addRoute('POST', '/send-email', [$staticPage, 'emailAction']);
         });
 
         return $dispatcher;
